d9a13482837e4ca50b68bb41e52cb3807233ae7f
Colin Putney <colin@bettercompany.co>
2017-12-06 16:18:58 -0800

    Use $ to indicate that a dependency should be lazy (#2)

--- wallaby.js
+++ wallaby.js
@@ -1,12 +1,15 @@
 module.exports = wallaby => {
   return {
-    files: ['index.js', 'src/**/*.js', 'test/*/*.js'],
+    files: ['index.js', 'src/**/*.js', 'test/config/*.json', 'test/*/*.js'],
     tests: ['test/*.js'],
     debug: true,
     testFramework: 'ava',
     env: {
       type: 'node',
-      runner: 'node'
+      runner: 'node',
+      params: {
+        env: 'NODE_ENV=test; TZ=UTC'
+      }
     }
   }
 }
